56633f563a055ee1335d7da904c8982ce5eeb8f7
Veda Hmath <159173596+Vedahmath@users.noreply.github.com>
2026-07-26 19:07:13 +0530

    Time: 1 ms (100.00%) | Memory: 42.8 MB (23.28%) - LeetSync

--- 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.java
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.java
@@ -1,14 +1,17 @@
 class Solution {
      public static int maxProduct(int n) {
-        int[] a = new int[10];
-        int c = 0, max = 0, k = 0;
+       
+        int c = 0, first = 0, second = 0;
         while (n > 0) {
             int r = n % 10;
-            a[c] = a[c] + r;
-            c++;
-            n = n / 10;
+            if(r > first){
+                second = first;
+                first = r;
+            }else if(r > second){
+                second = r;
+            }
+            n = n/10;
         }
-        Arrays.sort(a);
-        return a[a.length-1] * a[a.length-2];
-     }
+        return first * second;
+        }
 }
