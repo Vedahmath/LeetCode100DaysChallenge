a1c83e2c62ac319a19792e84c65a89f5596e84f6
Veda Hmath <159173596+Vedahmath@users.noreply.github.com>
2026-07-26 22:31:04 +0530

    Time: 1 ms (99.89%) | Memory: 55.6 MB (46.64%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,17 +1,18 @@
 class Solution {
     public int majorityElement(int[] nums) {
-       int freq=0,ans=0;
+      int freq =0, ans =0;
+      for(int i=0;i<nums.length;i++){
+            if(freq == 0) {
+                ans = nums[i];
+            }
+            if(ans == nums[i]){
+                freq++;
+            }else{
+                freq--;
+            }
+      }
+      
        
-       for(int i=0;i<nums.length;i++){
-        if(freq==0){
-            ans = nums[i];
-        }
-        if(ans == nums[i]){
-            freq++;
-        }else{
-            freq--;
-        }
-       }
        return ans;
     }
 }
