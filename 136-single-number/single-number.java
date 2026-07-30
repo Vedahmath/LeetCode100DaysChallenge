6e531dc5dc8c2f44bbd496c70d6b6243ddff1f85
Veda Hmath <159173596+Vedahmath@users.noreply.github.com>
2026-07-30 23:10:57 +0530

    Time: 12 ms (23.32%) | Memory: 47.6 MB (6.78%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,15 +1,15 @@
 class Solution {
     public int singleNumber(int[] nums) {
         Arrays.sort(nums);
-        int a = nums[0];
-        for(int i =1;i<nums.length;i++){
+        int a = nums[0],n=nums.length;
+        for(int i =1;i<n;i++){
                 if(i==0){
                 if(a != nums[i+1]) {
                     return a;
                 }
-                }else if(i<(nums.length-2) && nums[i] != nums[i-1] && nums[i] != nums[i+1]){
+                }else if(i<(n-2) && nums[i] != nums[i-1] && nums[i] != nums[i+1]){
                         a = nums[i];
-                }else if(i==(nums.length-1) && nums[i] !=nums[i-1] ){
+                }else if(i==(n-1) && nums[i] !=nums[i-1] ){
                     a= nums[i];
                 }
         }
